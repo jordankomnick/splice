7a78f85ef8fd0e67b7bdf3d1747c68563edf3df2
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2012-09-25 11:01:48 -0400

    indexOf exercise, with some test cases

--- splice/main.cpp
+++ splice/main.cpp
@@ -4,7 +4,29 @@
 
 using namespace std;
 
+double* arr;
+int arr_len;
+
+/**
+ * If h is an element of the array arr, then
+ * return its index. If not, return -1. So
+ * indexOf(arr+2) should return 2...
+ **/
+int indexOf(double* h){
+
+}
+
 int main(){
+	arr_len = 500;
+	arr = new double[arr_len];
+	double test;
+
+	cout << indexOf(&arr[15]) << endl;
+	cout << indexOf(&arr[1500]) << endl;
+	cout << indexOf(&test) << endl;
+	cout << indexOf(  (double*)(((char*)arr)+1)  ) << endl;
+
+	/*
 	List* l = new LinkedList();
 
 	l->pushFront(1.0);
@@ -19,6 +41,6 @@ int main(){
 		cout << l->getData(l->at(i));
 	}
 	cout << "]" << endl;
-
+	*/
 	return 0;
 }
