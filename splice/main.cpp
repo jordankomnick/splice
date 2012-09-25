3fca23660d785f46f17ea9c731c6839971ff53ce
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2012-09-25 09:20:45 -0400

    Update main to use polymorphism

--- splice/main.cpp
+++ splice/main.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 int main(){
-	ArrayList l;
+	List* l = new LinkedList();
 
-	l.pushFront(1.0);
-	l.pushFront(2.0);
-	l.pushFront(3.0);
+	l->pushFront(1.0);
+	l->pushFront(2.0);
+	l->pushFront(3.0);
 
 	cout << "[";
-	for(int i=0; l.at(i) != NULL; i++){
+	for(int i=0; l->at(i) != NULL; i++){
 		if(i != 0) {
 			cout << ", ";
 		}
-		cout << l.getData(l.at(i));
+		cout << l->getData(l->at(i));
 	}
 	cout << "]" << endl;
 
